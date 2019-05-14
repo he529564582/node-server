00e1afa5140f6673723c84290dba470d5d337157
hefang <he529564582@163.com>
2019-05-14 17:20:07 +0800

    new

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,12 +1,27 @@
  import _ from 'lodash';
+ import './style.css';
+ import Icon from './icon.png';
+ import Data from './data.xml';
+ import printMe  from './print.js';
 
-  function component() {
-    let element = document.createElement('div');
+ function component() {
+  var element = document.createElement('div');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  var btn = document.createElement('button');
 
-    return element;
-  }
+  // lodash 是由当前 script 脚本 import 导入进来的
+  element.innerHTML = _.join(['Hello', 'webpackApp'], ' ');
+  btn.innerHTML = '点击这里，然后查看 console！';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
-  document.body.appendChild(component());
++ element.classList.add('hello');
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  console.log(Data);
+
+  return element;
+}
+
+document.body.appendChild(component());
